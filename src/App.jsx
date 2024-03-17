0a2812c63cb2e4178548bfa843110e4cf2a1f11f
Mael Donnart <maeldonnart@hotmail.fr>
2024-03-17 16:22:17 +0100

    Incorporez du style et des assets à votre projet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from './components/Banner.jsx'
+import Cart from './components/Cart.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  )
+}
+
+export default App
